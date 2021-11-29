4dc3c7cff07be4b21761f29e807d4aab077c4f8f
Ryan Monteiro <ryan.monteiro@aluno.ufes.br>
2021-11-29 20:07:39 -0300

    Initial Commit

---
+++ src/main/java/br/ufes/App.java
@@ -0,0 +1,13 @@
+package br.ufes;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
